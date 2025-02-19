be53ca03c43efae55bbc996aa77482802f93e50d
Gui - Dev <gui.social.ti@gmail.com>
2025-02-19 20:10:11 -0300

    feat ✨: add first screen with logo and options

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,11 @@
 // This is a server component that will be rendered on the server side
 
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
 import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+
+import ConsumptionMethodOption from "./components/consuption-method-option";
 
 // Define the props for the RestaurantPage component
 interface RestaurantPageProps {
@@ -11,8 +16,48 @@ interface RestaurantPageProps {
 const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   const { slug } = await params;
   const restaurant = await getRestaurantBySlug(slug);
-  return <h1>{restaurant?.name}</h1>;
+
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center px-6 pt-24">
+      {/* LOGO E TITULO */}
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant?.avatarImageUrl}
+          alt={restaurant?.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant?.name}</h2>
+      </div>
+      {/* BEM VINDO*/}
+      <div className="pt-24 text-center space-y-2">
+        <h3 className="text-2xl font-semibold">Seja Bem vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos a oferecer
+          praticidade e sabor em cada detalhe
+        </p>
+      </div>
+      {/* METODO DE CONSUMO*/}
+      <div className="pt-14 grid grid-cols-2 gap-4">
+        {/* PARA COMER AQUI */}
+        <ConsumptionMethodOption
+          buttonText="Para comer aqui"
+          imageUrl="/dine_in.png"
+          imageAlt="Para comer aqui"
+        />
+        {/* PARA LEVAR */}
+        <ConsumptionMethodOption
+          buttonText="Para levar"
+          imageUrl="/takeaway.png"
+          imageAlt="Para levar"
+        />
+      </div>
+    </div>
+  );
 };
 
-// Export the RestaurantPage component as the default export
 export default RestaurantPage;
